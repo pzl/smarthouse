407fe3e41f1416b7d00a49daa025f09497df6b9c
pzl <dan@panzarel.la>
2015-06-10 09:11:32 -0400

    start main controller

--- controller.py
+++ controller.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 
+from smarthouse import *
+
+import sys
+
+def main():
+	w = Weather()
+	w.open(0,0,300,300)
+	w.fetch()
+	w.update()
+
+	sys.stdin.read(1)
+
+	w.close()
+
+
+if __name__ == "__main__":
+	main()
