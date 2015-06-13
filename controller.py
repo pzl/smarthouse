335b10b0b300a5b6c3085104fbc13aa514ed149f
pzl <dan@panzarel.la>
2015-06-13 09:47:21 -0400

    try multi-processing the modules

--- controller.py
+++ controller.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 
-from smarthouse import *
+from multiprocessing import Process
 
 import sys
+import time
 
-def main():
+def weather_widget():
+	from smarthouse import Weather
 	w = Weather()
 	w.open(5,5,300,300)
 	w.fetch()
 	w.update()
+	time.sleep(30)
+	w.close()
 
+
+
+def cal_widget():
+	from smarthouse import Calendar
 	c = Calendar()
 	c.open(330,5,500,300)
 	c.update()
+	time.sleep(30)
+	c.close()
 
-	"""
-	cl = Clock()
-	cl.open(320,0,250,20)
-	"""
+def main():
+	w = Process(target=weather_widget)
+	c = Process(target=cal_widget)
+	w.start()
+	c.start()
 
 	sys.stdin.read(1)
 
-	w.close()
-	c.close()
-	#cl.stop()
 
 
 if __name__ == "__main__":
